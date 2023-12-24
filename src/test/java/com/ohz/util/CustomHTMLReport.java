d658a1c4ee0f207fdc95a81bda3582125d25f0ad
Ozzy <o.mehmedovic@gmail.com>
2023-12-24 12:06:31 -0600

    Cleaned up HTML Report POJOs and removed Selenium dependency

--- src/test/java/com/ohz/util/CustomHTMLReport.java
+++ src/test/java/com/ohz/util/CustomHTMLReport.java
@@ -11,7 +11,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class CustomHTMLReport {
 
@@ -158,28 +157,13 @@ public class CustomHTMLReport {
     }
 }
 
-
-
 class Feature{
-    private String line;
     private ArrayList<Element> elements;
     private String name;
-    private String description;
-    private String keyword;
-    private String uri;
-
-    public String getLine() {
-        return line;
-    }
-
-    public void setLine(String line) {
-        this.line = line;
-    }
 
     public ArrayList<Element> getElements() {
         return elements;
     }
-
     public void setElements(ArrayList<Element> elements) {
         this.elements = elements;
     }
@@ -187,106 +171,26 @@ class Feature{
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
 }
 
 class Element {
-    private Date start_timestamp;
-    private int line;
     private String name;
-    private String description;
-    private String id;
-    private String type;
-    private String keyword;
     private ArrayList<Step> steps;
-
-    public Date getStart_timestamp() {
-        return start_timestamp;
-    }
-
-    public void setStart_timestamp(Date start_timestamp) {
-        this.start_timestamp = start_timestamp;
-    }
-
-    public int getLine() {
-        return line;
-    }
-
-    public void setLine(int line) {
-        this.line = line;
-    }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
     }
 
     public ArrayList<Step> getSteps() {
         return steps;
     }
-
     public void setSteps(ArrayList<Step> steps) {
         this.steps = steps;
     }
@@ -301,7 +205,6 @@ class Step{
     public ArrayList<String> getOutput() {
         return output;
     }
-
     public void setOutput(ArrayList<String> output) {
         this.output = output;
     }
@@ -309,7 +212,6 @@ class Step{
     public Result getResult() {
         return result;
     }
-
     public void setResult(Result result) {
         this.result = result;
     }
@@ -317,7 +219,6 @@ class Step{
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -325,28 +226,17 @@ class Step{
     public String getKeyword() {
         return keyword;
     }
-
     public void setKeyword(String keyword) {
         this.keyword = keyword;
     }
 }
 
 class Result{
-    private long duration;
     private String status;
-
-    public long getDuration() {
-        return duration;
-    }
-
-    public void setDuration(long duration) {
-        this.duration = duration;
-    }
 
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
